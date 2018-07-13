b98a37dfa65e3a583698623153bf809a081bf4bb
leonardo-mendes <leonardocm92@hotmail.com>
2018-07-13 10:53:19 -0300

    Implemented Consumer

--- src/main/kotlin/com/kafka/example/kafka/example/configurations/KafkaConfig.kt
+++ src/main/kotlin/com/kafka/example/kafka/example/configurations/KafkaConfig.kt
@@ -1,13 +1,17 @@
 package com.kafka.example.kafka.example.configurations
 
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.*
+import org.springframework.kafka.annotation.KafkaListener
+
+
 
 
 @Configuration
@@ -15,6 +19,9 @@ class KafkaConfig {
 
     @Value("\${kafka.bootstrap-address}")
     private lateinit var bootstrapAddress: String
+
+    @Value("\${kafka.bootstrap-group")
+    private lateinit var groupId: String
 
     @Bean
     open fun producerFactory(): ProducerFactory<String, String> =
@@ -29,4 +36,22 @@ class KafkaConfig {
     @Bean
     open fun kafkaTemplate(): KafkaTemplate<String, String> = KafkaTemplate(producerFactory())
 
+    private fun consumerFactory(): ConsumerFactory<String, String> =
+        mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapAddress,
+            ConsumerConfig.GROUP_ID_CONFIG to groupId,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java
+        ).let { DefaultKafkaConsumerFactory(it) }
+
+    @Bean
+    open fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> =
+        ConcurrentKafkaListenerContainerFactory<String, String>()
+            .apply { consumerFactory = consumerFactory() }
+
+
+    @KafkaListener(topics = ["rw_events"])
+    fun listen(message: String) {
+        println("Received Message from Kafka: $message")
+    }
 }
